ceb78674aada484008d8861401492df0cde773cd
Sebastian Alsina <alsinas@me.com>
2020-02-05 22:04:13 -0500

    createPool()
    changed createConnection() to createPool(), should avoid further crashes and removed like 10+ lines.

--- core.js
+++ core.js
@@ -3,23 +3,16 @@ const settings = require('./settings.json')
 
 exports.commands = {}
 
-var database = mysql.createConnection({
+exports.db = mysql.createPool({
     host: settings.database.host,
     user: settings.database.user,
     database: settings.database.database,
     password: settings.database.password
 })
 
-database.connect(function(err) {
-  if (err) throw err
-  console.log('Connection with database established.')
-})
-
-exports.db = database
-
 exports.createLedgerEntry = (user, amount, type, notes) => {
     var query = 'INSERT INTO ledger(user, amount, type, notes, timestamp) VALUES(?, ?, ?, ?, ?)'
-    database.query(query, [user, amount, type, notes, Date.now()], (err, results, fields) => {
+    this.db.query(query, [user, amount, type, notes, Date.now()], (err, results, fields) => {
         if (err) console.log(err)
 
         console.log(`Ledger record added: ${user}, ${amount}, ${type}, ${notes}`)
@@ -28,7 +21,7 @@ exports.createLedgerEntry = (user, amount, type, notes) => {
 
 exports.getSecondsSince = (user, action, callback) => {
     var query = 'SELECT timestamp FROM ledger WHERE user=? AND type=? ORDER BY timestamp DESC LIMIT 1'
-    database.query(query, [user, action], (err, results, fields) => {
+    this.db.query(query, [user, action], (err, results, fields) => {
         if (err) console.log(err)
         
         if (results.length) {
@@ -43,7 +36,7 @@ exports.getSecondsSince = (user, action, callback) => {
 
 exports.getBalance = (user, callback) => {
     var query = 'SELECT SUM(amount) AS balance FROM ledger WHERE user=?' 
-    database.query(query, [user], (err, results, fields) => {
+    this.db.query(query, [user], (err, results, fields) => {
         if (err) console.log(err)
 
         if (results[0].balance) {
